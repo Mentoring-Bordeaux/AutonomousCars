695e3efb109abce73e1e64c3fff687efb613840a
Christian Bonnaud <christian.bonnaud@avanade.com>
2023-09-26 14:03:00 +0200

    feat: Initialize CI project

---
+++ build/Build.cs
@@ -0,0 +1,70 @@
+using System.IO.Compression;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Pulumi;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Solution(GenerateProjects = true)]
+    readonly Solution Solution;
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src" / "api";
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    AbsolutePath ApiPackageDirectory => ArtifactsDirectory / "api";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("*/bin", "*/obj").ForEach(AbsolutePathExtensions.DeleteDirectory);
+            AbsolutePathExtensions.CreateOrCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(_ => _.SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Clean, Compile)
+        .Executes(() =>
+        {
+            DotNetPublish(_ => _
+                .SetProject(Solution.AutonomousCars_Api)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .SetOutput(ApiPackageDirectory));
+
+            ZipFile.CreateFromDirectory(ApiPackageDirectory, ArtifactsDirectory / "api.zip");
+        });
+
+}
