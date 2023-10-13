5de0c01bc52673103a2a688c75391f02e14f6a6b
Alexandre Nédélec <15186176+TechWatching@users.noreply.github.com>
2023-10-13 15:37:42 +0200

    Feature/base front app (#5)
    * feat(app): base front page and component
    
    * Reorganize files
    
    ---------
    
    Co-authored-by: Théo Gomichon <theo.gomichon@avanade.com>

---
+++ src/front/components/WeatherForecastDisplay.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { WeatherForecast } from "~/models/weatherForecast";
+const forecast = defineProps<WeatherForecast>();
+</script>
+
+<template>
+  <p>
+    {{ forecast.date }}: {{ forecast.summary }} ({{ forecast.temperatureC }}°C,
+    {{ forecast.temperatureF }}°F)
+  </p>
+</template>
+x²
---
+++ src/front/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { WeatherForecast } from '~/models/weatherForecast';
+
+const { data: forecasts } = await useFetch<WeatherForecast[]>(
+  "https://localhost:7238/weatherforecast"
+);
+</script>
+
+<template>
+  <h1>Weather forecasts</h1>
+  <WeatherForecast
+    v-for="forecast in forecasts"
+    :key="forecast.date"
+    :forecast="forecast"
+  />
+</template>
