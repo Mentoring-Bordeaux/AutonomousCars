a5477d6fbdd23e074023a9099fc08c8bbb174378
Alexandre Nedelec <15186176+TechWatching@users.noreply.github.com>
2023-10-25 22:56:45 +0200

    chore (front): Upgrade Nuxt to 3.8.0

--- src/front/components/WeatherForecastDisplay.vue
+++ src/front/components/WeatherForecastDisplay.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { WeatherForecast } from "~/models/weatherForecast";
+import type { WeatherForecast } from "~/models/weatherForecast";
 
 defineProps<{forecast: WeatherForecast}>();
 </script>
