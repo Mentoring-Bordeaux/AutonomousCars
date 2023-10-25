ccac72b6f1473fe01cb42c6ae2197b2d76fd087d
Alexandre Nedelec <15186176+TechWatching@users.noreply.github.com>
2023-10-25 22:56:45 +0200

    chore (front): Upgrade Nuxt to 3.8.0

--- src/front/nuxt.config.ts
+++ src/front/nuxt.config.ts
@@ -1,19 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   typescript: {
     strict: true,
   },
+
   modules: [
     '@unocss/nuxt',
   ],
+
   ssr: false,
+
   nitro: {
     devProxy: {
       "/api": {
         target: "https://localhost:7238/api/",
         secure: false
       },
+    }
+  },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
     }
   }
 })
