52e9cad7bd8f5feed55aa2a0de1730cc094043e3
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-10-27 22:02:11 +0200

    Copy telemetry emulator to AutonomousCars emulator

---
+++ src/emulator/PositionTelemetryProducer.cs
@@ -0,0 +1,13 @@
+﻿namespace AutonomousCars.Emulator;
+
+using GeoJSON.Text.Geometry;
+using MQTTnet.Client;
+using MQTTnet.Client.Extensions;
+
+internal class PositionTelemetryProducer : TelemetryProducer<Point>
+{
+    public PositionTelemetryProducer(IMqttClient mqttClient)
+        : base(mqttClient, new Utf8JsonSerializer(), "vehicles/{clientId}/position")
+    {
+    }
+}
---
+++ src/emulator/Worker.cs
@@ -0,0 +1,45 @@
+namespace AutonomousCars.Emulator;
+using GeoJSON.Text.Geometry;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Extensions;
+//https://github.com/dotnet/MQTTnet/blob/master/Samples/Client/Client_Publish_Samples.cs
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    private readonly IConfiguration _configuration;
+    public Worker(ILogger<Worker> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        MqttConnectionSettings cs = MqttConnectionSettings.CreateFromEnvVars(_configuration.GetValue<string>("envFile"));
+        _logger.LogInformation("Connecting to {cs}", cs);
+
+        IMqttClient mqttClient = new MqttFactory().CreateMqttClient(MqttNetTraceLogger.CreateTraceLogger());
+        mqttClient.DisconnectedAsync += e =>
+        {
+            _logger.LogInformation("Mqtt client disconnected with reason: {e}", e.Reason);
+            return Task.CompletedTask;
+        };
+
+        MqttClientConnectResult connAck = await mqttClient.ConnectAsync(new MqttClientOptionsBuilder().WithConnectionSettings(cs).Build(), stoppingToken);
+        _logger.LogInformation("Client {ClientId} connected: {ResultCode}", mqttClient.Options.ClientId, connAck.ResultCode);
+
+
+        PositionTelemetryProducer telemetryPosition = new(mqttClient);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            MqttClientPublishResult pubAck = await telemetryPosition.SendTelemetryAsync(
+                new Point(new Position(51.899523, -2.124156)), stoppingToken);
+            _logger.LogInformation("Message published with PUBACK {code} and mid {mid}", pubAck.ReasonCode, pubAck.PacketIdentifier);
+            await Task.Delay(5000, stoppingToken);
+        }
+
+    }
+}
