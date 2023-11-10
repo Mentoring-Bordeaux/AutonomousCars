ed4f9756ff4849f92c065f15f12eaddc5cf9fefd
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-11-10 17:54:25 +0100

    feat:init ressource group and webapp

--- infra/index.ts
+++ infra/index.ts
@@ -1,25 +1,50 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
-import * as storage from "@pulumi/azure-native/storage";
+import * as pulumi from "@pulumi/pulumi";
+import * as azure_native from "@pulumi/azure-native";
 
 // Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup", {
-    tags: { "Type": "demo"}
+const resourceGroup = new resources.ResourceGroup("rg-autonomouscars", {
+    location:"westeurope", 
+    resourceGroupName:"rg-autonomouscars"
 });
 
-// Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
-    },
-    kind: storage.Kind.StorageV2,
+
+
+const webApp = new azure_native.web.WebApp("stapp-autonomouscars", {
+    // cloningInfo: {
+    //     appSettingsOverrides: {
+    //         Setting1: "NewValue1",
+    //         Setting3: "NewValue5",
+    //     },
+    //     cloneCustomHostNames: true,
+    //     cloneSourceControl: true,
+    //     configureLoadBalancing: false,
+    //     hostingEnvironment: "/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/testrg456/providers/Microsoft.Web/hostingenvironments/aseforsites",
+    //     overwrite: false,
+    //     sourceWebAppId: "/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/testrg456/providers/Microsoft.Web/sites/srcsiteg478",
+    //     sourceWebAppLocation: "West Europe",
+    // },
+    // kind: "app",
+    location: resourceGroup.location,
+    // name: "sitef6141",
+    resourceGroupName: resourceGroup.name
 });
 
-// Export the primary key of the Storage Account
-const storageAccountKeys = storage.listStorageAccountKeysOutput({
-    resourceGroupName: resourceGroup.name,
-    accountName: storageAccount.name
-});
 
-export const primaryStorageKey = pulumi.secret(storageAccountKeys.keys[0].value);
+// const staticSite = new azure_native.web.StaticSite("stapp-autonomouscars", {
+//     branch: "main",
+//     // buildProperties: {
+//     //     apiLocation: "api",
+//     //     appArtifactLocation: "build",
+//     //     appLocation: "app",
+//     // },
+//     location,
+//     name: "stapp-autonomouscars",
+//     // repositoryToken: "repoToken123",
+//     // repositoryUrl: "https://github.com/username/RepoName",
+//     resourceGroupName: resourceGroup.name,
+//     // sku: {
+//     //     name: "Basic",
+//     //     tier: "Basic",
+//     // },
+// });
