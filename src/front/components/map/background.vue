336df61b18cf3f4f686cc5d87f9d1261ffabdf5d
BOUR Florian <Florian.Bour@bordeaux-inp.fr>
2023-11-10 17:33:30 +0100

    Changed export default to onMounted function

--- src/front/components/map/background.vue
+++ src/front/components/map/background.vue
@@ -1,28 +1,20 @@
-<script lang="ts">
+<script  setup lang="ts">
     import * as atlas from "azure-maps-control";
     import "azure-maps-control/dist/atlas.min.css";
-import { LanguageServiceMode } from "typescript";
 
-    export default {
-        mounted() {this.initializeMap()}, 
-        methods: {
-            initializeMap() {
-                const map = new atlas.Map("map", {
-                    view: "Auto",
-                    language: "fr-FR",
-                    center: [-0.604945545248392, 44.806516403595744],
-                    zoom: 10,
-                    authOptions: {
-                        authType: "subscriptionKey" as atlas.AuthenticationType,
-                        subscriptionKey: "76c6F3ZTQ9iJDRo20oAU-D5YtyP7wkG-s1BRffLLgbc"
-                    }
-                });
+    onMounted(() => {
+        const map = new atlas.Map("map", {
+            view: "Auto",
+            language: "fr-FR",
+            center: [-0.604945545248392, 44.806516403595744],
+            zoom: 10,
+            authOptions: {
+                authType: "subscriptionKey",
+                subscriptionKey: "76c6F3ZTQ9iJDRo20oAU-D5YtyP7wkG-s1BRffLLgbc"
             }
-        }
-    }
-    
+        });
+    });
 
-    
 </script>
 
 <template>
