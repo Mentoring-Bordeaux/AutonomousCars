634166090af5cfcb60110be5680cd44b66aee498
BOUR Florian <Florian.Bour@bordeaux-inp.fr>
2023-11-10 16:37:20 +0100

    Initialized Azure Map + changed cover page to integrate the map

---
+++ src/front/components/map/background.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import * as atlas from "azure-maps-control";
+    import "azure-maps-control/dist/atlas.min.css";
+import { LanguageServiceMode } from "typescript";
+
+    export default {
+        mounted() {this.initializeMap()}, 
+        methods: {
+            initializeMap() {
+                const map = new atlas.Map("map", {
+                    view: "Auto",
+                    language: "fr-FR",
+                    center: [-0.604945545248392, 44.806516403595744],
+                    zoom: 10,
+                    authOptions: {
+                        authType: "subscriptionKey" as atlas.AuthenticationType,
+                        subscriptionKey: "76c6F3ZTQ9iJDRo20oAU-D5YtyP7wkG-s1BRffLLgbc"
+                    }
+                });
+            }
+        }
+    }
+    
+
+    
+</script>
+
+<template>
+    <div id="map" class="w-screen h-screen p-0 m-0"></div>
+</template>
--- src/front/pages/index.vue
+++ src/front/pages/index.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
-import type { WeatherForecast } from '~/models/weatherForecast';
-
-const { data: forecasts } = await useFetch<WeatherForecast[]>(
-  "api/weatherforecast"
-);
 </script>
 
 <template>
-  <h1>Weather forecasts</h1>
-  <div v-for="forecast in forecasts" :key="forecast.date">
-    <WeatherForecastDisplay :forecast="forecast"/>
-  </div>
+  <MapBackground></MapBackground>
 </template>
