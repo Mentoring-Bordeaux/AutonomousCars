5b218f3f47e87eb6ea0814bf5891c526cfb17ba9
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-11-17 14:47:08 +0100

    [feat] Add role assignement for the API

--- infra/index.ts
+++ infra/index.ts
@@ -1,5 +1,8 @@
 import * as resources from "@pulumi/azure-native/resources";
 import * as azure_native from "@pulumi/azure-native";
+import * as pulumi from "@pulumi/pulumi";
+import { ManagedServiceIdentityType } from "@pulumi/azure-native/app";
+import { PrincipalType } from "@pulumi/azure-native/authorization";
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("rg-autonomouscars", {
@@ -40,4 +43,15 @@ const webApp = new azure_native.web.WebApp("app-autonomouscars", {
     kind: "app",
     location: resourceGroup.location,
     resourceGroupName: resourceGroup.name,
+    identity: {
+        type: ManagedServiceIdentityType.SystemAssigned
+    }
 });
+
+// Assign Azure Maps Data Reader role to a Principal
+const roleAssignment = new azure_native.authorization.RoleAssignment("ra-autonomouscars", {
+    principalId: webApp.identity.apply(x => x?.principalId!),
+    roleDefinitionId: "providers/Microsoft.Authorization/roleDefinitions/423170ca-a8f6-4b0f-8487-9e4eb8f49bfa",
+    scope: mapsAccount.id,
+    principalType: PrincipalType.ServicePrincipal,
+}); 
