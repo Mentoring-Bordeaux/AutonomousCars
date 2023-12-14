2cb7384654857ecd1ed21edb12399f64ec2f758b
tgaignard001 <tgaignard001@bordeaux-inp.fr>
2023-12-14 18:42:21 +0100

    working display car with local azure circle function

---
+++ src/azure_function/circle_emulator/Function.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace csharp_isolated;
+
+public class Functions
+{
+    private static readonly HttpClient HttpClient = new();
+
+    // itinerary to make a circle in Bordeaux France
+    private static readonly double[][] Coordinates = [[44.820900, -0.540887], [44.816910, -0.549084], [44.812411, -0.557402], [44.814767, -0.572660], [44.821979, -0.584090], [44.831374, -0.598355], [44.843274, -0.599613], [44.849691, -0.596497], [44.860680, -0.588156], [44.869431, -0.566570], [44.860315, -0.553862], [44.853657, -0.566392], [44.849896, -0.570178], [44.840642, -0.568950], [44.830790, -0.555252]];
+    private static int IndexPosition = 0;
+
+    [Function("negotiate")]
+    public static HttpResponseData Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData req,
+        [SignalRConnectionInfoInput(HubName = "serverless")] string connectionInfo)
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "application/json");
+        response.WriteString(connectionInfo);
+        return response;
+    }
+
+    [Function("broadcast")]
+    [SignalROutput(HubName = "serverless")]
+    public static SignalRMessageAction Broadcast([TimerTrigger("*/5 * * * * *")] TimerInfo timerInfo)
+    {
+        double[] toSend = Coordinates[IndexPosition];
+        IndexPosition++;
+        IndexPosition %= Coordinates.Length;
+        return new SignalRMessageAction("newMessage", [toSend]);
+    }
+}
--- src/front/components/map/background.vue
+++ src/front/components/map/background.vue
@@ -1,28 +1,55 @@
 <script setup lang="ts">
 import * as atlas from "azure-maps-control";
 import "azure-maps-control/dist/atlas.min.css";
+import * as signalR from "@microsoft/signalr"
+
+const apiBaseUrl = "http://localhost:7071";
+const carPath = "/img/car.png"
+
 
 onMounted(() => {
-  const config = useRuntimeConfig();
-  const { getMapToken } = useAzureMaps();
+	const config = useRuntimeConfig();
+	const { getMapToken } = useAzureMaps();
 
-  const map = new atlas.Map("map", {
-    view: "Auto",
-    language: "fr-FR",
-    center: [-0.604945545248392, 44.806516403595744],
-    zoom: 10,
-    authOptions: {
-      authType: atlas.AuthenticationType.anonymous,
-      clientId: config.public.azureMapsClientId,
-      getToken: (resolve, reject) =>
-        getMapToken()
-          .then(({ token }) => resolve(token))
-          .catch((error) => reject(error)),
-    },
-  });
+
+	const map = new atlas.Map("map", {
+		view: "Auto",
+		language: "fr-FR",
+		center: [-0.604945545248392, 44.806516403595744],
+		zoom: 10,
+		authOptions: {
+			authType: atlas.AuthenticationType.anonymous,
+			clientId: config.public.azureMapsClientId,
+			getToken: (resolve, reject) =>
+			getMapToken()
+			.then(({ token }) => resolve(token))
+			.catch((error) => reject(error)),
+		},
+	});
+
+	//Wait until the map resources are ready.
+	map.events.add('ready', function () {
+		var car = new atlas.HtmlMarker({
+			position: [-0.607294, 44.806267],
+			htmlContent: '<img src="' + carPath + '" style="width: 50px; height: 30px;" />'
+		})
+		map.markers.add(car)
+
+		const connection = new signalR.HubConnectionBuilder()
+		.withUrl(apiBaseUrl + '/api')
+		.configureLogging(signalR.LogLevel.Information)
+		.build();
+		connection.on('newMessage', (message: atlas.data.Position) => {
+			car.setOptions({
+				position: message.reverse()
+			})
+		});
+		connection.start()
+		.catch(console.error);
+	});
 });
 </script>
 
 <template>
-  <div id="map" class="h-[calc(100vh-4rem-12px)] w-screen"></div>
+	<div id="map" class="h-[calc(100vh-4rem-12px)] w-screen"></div>
 </template>
