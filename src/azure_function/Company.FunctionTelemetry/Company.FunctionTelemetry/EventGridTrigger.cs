c7b340c0b2ee50c35f7b73b19cdeb7a7edc894c0
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-12-15 15:32:14 +0100

    [fix] Modifiy function with cloud event

--- src/azure_function/Company.FunctionTelemetry/Company.FunctionTelemetry/EventGridTrigger.cs
+++ src/azure_function/Company.FunctionTelemetry/Company.FunctionTelemetry/EventGridTrigger.cs
@@ -1,19 +1,25 @@
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 
+using Azure.Messaging;
 using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-
 namespace Company.FunctionTelemetry;
 
 public static class EventGridTrigger
 {   
     [Function("EventGridTrigger")]
-    public static void Run([EventGridTrigger] Position position, FunctionContext context)
+    [SignalROutput(HubName = "chat", ConnectionStringSetting = "SignalRConnection")]
+    public static SignalRMessageAction Run([EventGridTrigger] CloudEvent position, FunctionContext context)
     {
         var logger = context.GetLogger("EventGridTrigger");
-        logger.LogInformation("New point : {x}, {y}", position.Coordinates[0], position.Coordinates[1]);
+        logger.LogInformation("Position received : {message}", position.Data.ToString());
+        return new SignalRMessageAction("newPosition")
+        {
+            // broadcast to all the connected clients without specifying any connection, user or group.
+            Arguments = new []{position},
+        };
     }
 }
 
