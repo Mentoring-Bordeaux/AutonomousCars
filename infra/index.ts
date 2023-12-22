2bb5c47fbc403e2208f53e5f146e2cf6516c32e8
theogom <theo.gom.2000@gmail.com>
2023-12-22 13:51:33 +0100

    feat(infra): set azure maps client id in api app settings

--- infra/index.ts
+++ infra/index.ts
@@ -5,19 +5,19 @@ import { PrincipalType } from "@pulumi/azure-native/authorization";
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("rg-autonomouscars", {
-    location:"westeurope", 
-    resourceGroupName:"rg-autonomouscars"
+  location: "westeurope",
+  resourceGroupName: "rg-autonomouscars",
 });
 
 // Create a Static Web App
 const staticWebApp = new azure_native.web.StaticSite("stapp-autonomouscars", {
-    location: resourceGroup.location,
-    resourceGroupName: resourceGroup.name,
-    branch: "main", 
-    sku: {
-        name: "Free",
-        tier: "Free",
-    }
+  location: resourceGroup.location,
+  resourceGroupName: resourceGroup.name,
+  branch: "main",
+  sku: {
+    name: "Free",
+    tier: "Free",
+  },
 });
 
 // Export the URL of the Static Web App
@@ -25,32 +25,44 @@ export const appUrl = staticWebApp.defaultHostname;
 
 // Create an Azure Maps Account
 const mapsAccount = new azure_native.maps.Account("maps-autonomouscars", {
-    accountName: "maps-autonomouscars",
-    kind: "Gen2",
-    location: resourceGroup.location,
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: "G2",
-    },
-    tags: {
-        test: "true",
-    },
+  accountName: "maps-autonomouscars",
+  kind: "Gen2",
+  location: resourceGroup.location,
+  resourceGroupName: resourceGroup.name,
+  sku: {
+    name: "G2",
+  },
+  tags: {
+    test: "true",
+  },
 });
 
 // Create an App Service for the API
 const webApp = new azure_native.web.WebApp("app-autonomouscars", {
-    kind: "app",
-    location: resourceGroup.location,
-    resourceGroupName: resourceGroup.name,
-    identity: {
-        type: ManagedServiceIdentityType.SystemAssigned
-    }
+  kind: "app",
+  location: resourceGroup.location,
+  resourceGroupName: resourceGroup.name,
+  identity: {
+    type: ManagedServiceIdentityType.SystemAssigned,
+  },
+  siteConfig: {
+    appSettings: [
+      {
+        name: "AzureMaps:ClientId",
+        value: mapsAccount.properties.uniqueId,
+      },
+    ],
+  },
 });
 
-// Assign Azure Maps Data Reader role to a Principal
-const roleAssignment = new azure_native.authorization.RoleAssignment("ra-autonomouscars", {
-    principalId: webApp.identity.apply(x => x?.principalId!),
-    roleDefinitionId: "providers/Microsoft.Authorization/roleDefinitions/423170ca-a8f6-4b0f-8487-9e4eb8f49bfa",
+// Assign Azure Maps Data Reader role to API managed identity
+const roleAssignment = new azure_native.authorization.RoleAssignment(
+  "ra-autonomouscars",
+  {
+    principalId: webApp.identity.apply((x) => x?.principalId!),
+    roleDefinitionId:
+      "providers/Microsoft.Authorization/roleDefinitions/423170ca-a8f6-4b0f-8487-9e4eb8f49bfa",
     scope: mapsAccount.id,
-    principalType: PrincipalType.ServicePrincipal
-}); 
+    principalType: PrincipalType.ServicePrincipal,
+  }
+);
