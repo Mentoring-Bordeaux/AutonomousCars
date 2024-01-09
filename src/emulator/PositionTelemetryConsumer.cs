c34eb02668e7b6146e900a61ad0750bb9bbbd0f3
tgaignard001 <tgaignard001@bordeaux-inp.fr>
2024-01-09 09:52:10 +0100

    emulator with status + speed check and one job running

--- src/emulator/PositionTelemetryConsumer.cs
+++ src/emulator/PositionTelemetryConsumer.cs
@@ -1,10 +1,11 @@
 namespace Emulator;
 
 using GeoJSON.Text.Geometry;
+using GeoJSON.Text.Feature;
 using MQTTnet.Client;
 using MQTTnet.Client.Extensions;
 
-public class PositionTelemetryConsumer : TelemetryConsumer<LineString>
+public class PositionTelemetryConsumer : TelemetryConsumer<Feature<LineString>>
 {
     public PositionTelemetryConsumer(IMqttClient mqttClient)
         : base(mqttClient, new Utf8JsonSerializer(), $"itinerary/{mqttClient.Options.ClientId}")
--- src/emulator/Worker.cs
+++ src/emulator/Worker.cs
@@ -1,5 +1,6 @@
 namespace Emulator;
 
+using GeoJSON.Text.Feature;
 using Model;
 using GeoJSON.Text.Geometry;
 using MQTTnet;
@@ -21,53 +22,104 @@ public class Worker : BackgroundService
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        MqttConnectionSettings cs = MqttConnectionSettings.CreateFromEnvVars(_configuration.GetValue<string>("envFile"));
+        var cs = MqttConnectionSettings.CreateFromEnvVars(_configuration.GetValue<string>("envFile"));
         _logger.LogInformation("Connecting to {cs}", cs);
 
-        IMqttClient mqttClient = new MqttFactory().CreateMqttClient(MqttNetTraceLogger.CreateTraceLogger());
+        var mqttClient = new MqttFactory().CreateMqttClient(MqttNetTraceLogger.CreateTraceLogger());
         mqttClient.DisconnectedAsync += e =>
         {
             _logger.LogInformation("Mqtt client disconnected with reason: {e}", e.Reason);
             return Task.CompletedTask;
         };
 
-        MqttClientConnectResult connAck = await mqttClient.ConnectAsync(new MqttClientOptionsBuilder().WithConnectionSettings(cs).Build(), stoppingToken);
+        var connAck = await mqttClient.ConnectAsync(new MqttClientOptionsBuilder().WithConnectionSettings(cs).Build(), stoppingToken);
         _logger.LogInformation("Client {ClientId} connected: {ResultCode}", mqttClient.Options.ClientId, connAck.ResultCode);
-        
-        
         
         PositionTelemetryProducer telemetryProducer = new(mqttClient);
         PositionTelemetryConsumer telemetryConsumer = new(mqttClient)
         {
-            OnTelemetryReceived = HandleRequest(telemetryProducer, stoppingToken)
+            OnTelemetryReceived = HandleRequest(telemetryProducer, mqttClient.Options.ClientId, stoppingToken)
         };
         await telemetryConsumer.StartAsync();
     }
-    
+
     private const int Speed = 500; // Arbitrary number to send messages
-    
-    private Action<TelemetryMessage<LineString>> HandleRequest(PositionTelemetryProducer telemetryProducer, CancellationToken stoppingToken)
+    private Position _lastLocationKnown = new(44.84416, -0.57859); // Arbitrary location to simulate geolocation. 
+    private readonly SemaphoreSlim _handleSemaphore = new(1, 1);
+
+    private Action<TelemetryMessage<Feature<LineString>>> HandleRequest(PositionTelemetryProducer telemetryProducer, string clientId, CancellationToken stoppingToken)
     {
         return Handle;
         
-        async void Handle(TelemetryMessage<LineString> request)
+        async void Handle(TelemetryMessage<Feature<LineString>> request)
         {
-            _logger.LogInformation("Received request from {from}", request.ClientIdFromTopic);
-         
-            
-            if (request.Payload!.Coordinates.Count > 0)
+            try
             {
-                foreach (var point in request.Payload.Coordinates)
+                if (await _handleSemaphore.WaitAsync(TimeSpan.Zero, stoppingToken))
                 {
-                    await telemetryProducer.SendTelemetryAsync(new Point(new Position(point.Latitude, point.Longitude)), stoppingToken);
-                    _logger.LogInformation("Time to wait {time}", Speed);
-                    await Task.Delay(Speed, stoppingToken);
+                    try
+                    {
+                        _logger.LogInformation("Received request from {from}", request.ClientIdFromTopic);
+                        var data = request.Payload!.Geometry;
+                        var properties = request.Payload.Properties;
+
+                        if (request.ClientIdFromTopic != clientId)
+                        {
+                            _logger.LogInformation("Request from {clientId} but I am {myId}", request.ClientIdFromTopic, clientId);
+                            return;
+                        }
+
+                        if (data != null && data.Coordinates.Count > 0)
+                        {
+                            foreach (var point in data.Coordinates)
+                            {
+                                var carSpeed = Speed;
+                                if (properties.TryGetValue("speed", out var newCarSpeed) && int.TryParse(newCarSpeed.ToString(), out var parsedSpeed))
+                                {
+                                    carSpeed = parsedSpeed;
+                                }
+                    
+                                var position = new Position(point.Latitude, point.Longitude);
+                    
+                                await telemetryProducer.SendTelemetryAsync(new Point(position), stoppingToken);
+                                _lastLocationKnown = position;
+                    
+                                _logger.LogInformation("Time to wait {time}", carSpeed);
+                                await Task.Delay(carSpeed, stoppingToken);
+                            }
+                        }
+                        else
+                        {
+                            if (properties != null && properties.TryGetValue("status", out var isTestValue) && isTestValue.ToString()!.ToLower().Equals("true"))
+                            {
+                                await telemetryProducer.SendTelemetryAsync(new Point(_lastLocationKnown), stoppingToken);
+                                _logger.LogInformation("Ask for {clientId} status", request.ClientIdFromTopic);
+                            }
+                            else if (data != null)
+                            {
+                                SendDefaultPositions(telemetryProducer, stoppingToken);
+                            }
+                            else
+                            {
+                                _logger.LogInformation("Not a good format");
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        _handleSemaphore.Release();
+                    }
+                }
+                else
+                {
+                    _logger.LogInformation("Unable to emulate: Another job is currently running");
                 }
             }
-            else
+            catch (Exception ex)
             {
-               SendDefaultPositions(telemetryProducer, stoppingToken);
+                _logger.LogError(ex, "An error occurred in Handle.");
             }
+            
         }
     }
 
