5935bcd4c03cb5282f149ecc4571fceb727ea7d5
Rebecca <rebeccapinoteau@gmail.com>
2024-01-10 20:01:52 +0100

    [fix] resolve tabs

--- src/front/components/vehicles/cardList.vue
+++ src/front/components/vehicles/cardList.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
-import type { Vehicle } from '~/models/Vehicle';
 
-const enum Availability {
-    All,
-    Available,
-    Unavailable
-}
-const props = defineProps({'type':Number});
-
-const shouldDisplayVehicle = (vehicle: Vehicle): boolean => {
-  return props.type === Availability.All 
-         || (props.type === Availability.Available && vehicle.available)
-         || (props.type === Availability.Unavailable && !vehicle.available);
-};
-
-var vehiclesList = useVehiclesListStore().vehiclesList
+const props = defineProps(['vehiclesList']);
 </script>
+
 <template>
-    <div v-for="vehicle in vehiclesList">
-        <VehiclesCard v-if="shouldDisplayVehicle(vehicle)" :vehicle=vehicle></VehiclesCard>
-    </div>
+  <div v-for="vehicle in vehiclesList">
+    <VehiclesCard :vehicle="vehicle"></VehiclesCard>
+  </div>
 </template>
--- src/front/components/vehicles/content.vue
+++ src/front/components/vehicles/content.vue
@@ -1,37 +1,48 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import type { Vehicle } from '~/models/Vehicle';
 
-const tabs = ref(['Tous', 'Disponible', 'Indisponible']);
-const tabContents = ref(['', '', '']);  
-const activeTab = ref(0);
+const items = [
+  { key: 'all', label: 'Tous' },
+  { key: 'available', label: 'Disponible' },
+  { key: 'unavailable', label: 'Indisponible' },
+];
 
-function changeTab(index: number) {
-  activeTab.value = index;
-}
+const vehiclesListStore = useVehiclesListStore();
+
+const shouldDisplayVehicle = (item: { key: string }) => (vehicle: Vehicle): boolean => {
+  return (
+    item.key === 'all' ||
+    (item.key === 'available' && vehicle.available) ||
+    (item.key === 'unavailable' && !vehicle.available)
+  );
+};
+
+const filteredVehiclesList = (item: { key: any; }) => {
+  if (item.key === 'all') {
+    return vehiclesListStore.vehiclesList;
+  }
+  return vehiclesListStore.vehiclesList.filter(shouldDisplayVehicle(item));
+};
 </script>
-<template>
-    <div class="bg-gray-100 p-4 shadow">
-      <div class="flex items-center">
-        <nuxt-link :to="{ path: '/'}" class="text-gray-600 hover:text-gray-800 focus:outline-none">
-          <i class="i-heroicons-chevron-left"></i>
-        </nuxt-link>
-        <h1 class="text-xl text-[#E36C39] font-400 ml-4">Les véhicules</h1>
-      </div>
-    </div>
 
-    <div class="py-3">
-      <ul class="hidden text-xs font-medium text-center text-gray-500 rounded-xl shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
-        <li v-for="(tab, index) in tabs" :key="index" class="w-full">
-          <a @click="changeTab(index)" :class="{ 'text-gray-900 bg-gray-100': activeTab === index, 'bg-white hover:text-gray-700 hover:bg-gray-50': activeTab !== index }" class="inline-block w-full p-3 border-r border-gray-200 dark:border-gray-700 rounded-s-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">{{ tab }}</a>
-        </li>
-      </ul>
+<template>
+  <div class="bg-gray-100 p-4 shadow">
+    <div class="flex items-center">
+      <nuxt-link :to="{ path: '/'}" class="text-gray-600 hover:text-gray-800 focus:outline-none">
+        <i class="i-heroicons-chevron-left"></i>
+      </nuxt-link>
+      <h1 class="text-xl text-[#E36C39] font-400 ml-4">Les véhicules</h1>
     </div>
-      <div v-if=useVehiclesListStore().isLoaded v-for="(_, index) in tabContents" :key="index" v-show="activeTab === index" class="h-125 overflow-y-scroll">
-        <VehiclesCardList :type="index"></VehiclesCardList>
+  </div>
+
+  <UTabs :items="items" class="w-full mt-3">
+    <template #item="{ item }">
+      <div v-if="vehiclesListStore.isLoaded" class="space-y-3">
+        <VehiclesCardList :vehiclesList="filteredVehiclesList(item)"></VehiclesCardList>
       </div>
-      <div v-else > 
+      <div v-else>
         <VehiclesLoader></VehiclesLoader>
       </div>
-  </template>
-
-  
+    </template>
+  </UTabs>
+</template>
